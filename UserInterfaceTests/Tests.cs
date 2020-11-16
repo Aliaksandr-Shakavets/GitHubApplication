dd27b02047f164d8ca9fb186e352b2c7d4b82640
Aliaksandr Shakavets <aliaksandr.shakavets@gmail.com>
2020-11-17 01:06:03 +0300

    Preparing to develope. Installing packages

---
+++ UserInterfaceTests/Tests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UserInterfaceTests
+{
+    public class Tests
+    {
+        private IWebDriver webDriver;
+
+        [OneTimeSetUp]
+        public void RunBeforeAnyTest()
+        {
+            webDriver = new ChromeDriver();
+            webDriver.Manage().Window.Maximize();
+        }
+
+        [OneTimeTearDown]
+        public void RunAfterAnyTests()
+        {
+            webDriver.Close();
+        }
+    }
+}
